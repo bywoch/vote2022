a6000d1ecc649ae9bee42efadab56fed6612a788
Bright Chung <bywoch@gmail.com>
2021-10-13 17:00:00 +0900

    vote_map 02

--- vote_map/js/vote.open.shell2.js
+++ vote_map/js/vote.open.shell2.js
@@ -134,17 +134,14 @@ function cleanOpenParam(type) {
                     $('.select-do').append(mainHtml);
                     $('.select-bx .vote-area-items-all').remove();
                     /* ↑ 대선추가 ↑ */
-                    $mapIcon = $('.select-do .vote-area-items-body').first(); //전국 아이콘 선택 
-                    $sidoList = $('.select-do .vote-area-list');
+                    $mapIcon = $('.vote-area-items-body').first(); //전국 아이콘 선택 
+                    $sidoList = $('.vote-area-list');
                     $sidoItems = $sidoList.children('.vote-area-items');
                     $sidoArea = $sidoItems.find('.area');
                     $sidoArea.on('click', function (e) {
                         e.preventDefault();
                         $('.vote-area .vote-area-items-body').hide(); //시도 숨기기
                         $mapIcon.show(); //전국 아이콘만 보이게
-                        sidotitle = '';
-                        sidotitle += '<strong class="border">구·시·군</strong><span>(구·시·군을 선택해주세요)</span>';
-                        $('.now-map-h4').html(sidotitle);
                         $index = $(this).attr('data-nav');
                         $apiUrl = $(this).attr('href');
                         setOpenParam("sido", $(this).attr('data-nav'), $(this).attr('data-nav-nm'));
@@ -165,52 +162,17 @@ function cleanOpenParam(type) {
                         var returnDong = '읍·면·동 ▼'
                         $('.select-dong > li').empty().append(returnDong);
                         /* ↑ 대선추가 ↑ */
-                        //전국 아이콘 클릭시
-                        if ($index == 0) {
-                            voteUIMap.map(); //그림만 그려줌
-                            $('.vote-area .vote-area-items-body').show();
-                            sidotitle = '';
-                            sidotitle += '<strong class="border">시·도</strong><span>(시·도를 선택해주세요)</span>';
-                            $('.now-map-h4').html(sidotitle);
-                            $mapIcon.hide();
-                            $('.area-gu-list').remove();
-                            $('.area-dong-body').remove();
-                            cleanOpenParam("sido");
-                            cleanOpenParam("gu");
-                            cleanOpenParam("dong");
-                            //voteUIMap.setOpenCleanSelBoxSetting('sg');
-                            getAllGePyoRate();
-                            /* ※ ↓ 투개표 진행시 주석해제 ↓ ※ */
-                            /*var initHTML = '<div class="wrap-rank-tit">' + 
-                                '<div class="cont-none">' +
-                                '1.<span class="purple"> 좌측지도</span>에서 확인하고자 하는 <span class="purple">지역을 선택</span>해주세요.' +
-                                '<br>2. 상단의 <span class="purple">선택박스</span>에서 <span class="purple">선거구분을 선택</span>한 후,' +
-                                '<br><span class="purple">조회 버튼</span>을 클릭해주세요.' +
-                                '</div>' +
-                                '</div>';
-                            $("#LOCALOPENHUBOLIST").html(initHTML);*/
-                            $('span.data-place').text('전국');
-
-                            /* ↓ 대선추가 ↓ */
-                            var returnDo = '시·도 ▼<span>&#62;</span>'
-                            $('.select-do > li').empty().append(returnDo);
-                            var returnGu = '구·시·군 ▼<span>&#62;</span>'
-                            $('.select-gu > li').empty().append(returnGu);
-                            var returnDong = '읍·면·동 ▼'
-                            $('.select-dong > li').empty().append(returnDong);
-                            /* ↑ 대선추가 ↑ */
-                            return false;
-                        }
                         voteUIMap.setGu($apiUrl, $index);
                         /*voteUIMap.setSgTypeSelBox();*/ //지방선거 선거구분
                         setNowLocatioInfoAndGePyoRate(paramOpen.sido.name, $(this).attr('data-nav-rate'));
                     });
                     /* ↓ 대선추가 ↓ */
-                    $('.vote-map .vote-area-items').click(function (e) {
-                        e.preventDefault();
-                    });
+                    // $('.vote-map .vote-area-items').click(function (e) {
+                    //     e.preventDefault();
+                    // });
                     $('.select-district .data-place').on('click', function (e) {
                         e.preventDefault();
+                        $('#dataPlace').text('전국');
                         $mapIcon.hide();
                         voteUIMap.map(); //그림만 그려줌
                         $('.vote-area-items-body').show();
@@ -288,7 +250,7 @@ function cleanOpenParam(type) {
                     /* ↓ 대선추가 ↓ */
                     $('.select-gu').append(mainHtml);
                     /* ↑ 대선추가 ↑ */
-                    $guItems = $('.select-gu .area-gu-items');
+                    $guItems = $('.area-gu-items');
                     $guArea = $guItems.find('.area-gu');
                     $guArea.on('click', function (e) {
                         e.preventDefault();
@@ -398,88 +360,7 @@ function cleanOpenParam(type) {
                     }
                 }
             }
-        },
-        /* ↓ 지방선거 선거구분 ↓ */
-        /*setSgTypeSelBox: function () {
-            $.ajax({
-                type: 'get',
-                url: 'https://vote2018.imbc.com/api/GetSgTypeList',
-                dataType: 'jsonp',
-                data: {
-                    sidonm: paramOpen.sido.name,
-                    wiwnm: paramOpen.gu.name,
-                    emdid: paramOpen.dong.id
-                },
-                jsonp: 'callback',
-                success: function (data) {
-
-                    //voteUIMap.setOpenCleanSelBoxSetting('sg');
-
-                    var szHtml = [];
-
-                    if (data.length == 0) {
-                        szHtml.push("<li><button type=\"button\" class=\"button button-item\">해당 선거 정보가 없습니다</button></li>");
-                    } else {
-                        if (data[0].SGTYPECODE == 2) {
-                            for (var i = 1; i < data.length + 1; i++) {
-                                if (i == data.length) {
-                                    szHtml.push("<li><button type=\"button\" class=\"button button-item\" data-set=\"" + data[0].SGTYPECODE + "\">" + voteUIMap.setSgTypeName(data[0].SGTYPECODE) + "</button></li>");
-                                } else {
-                                    szHtml.push("<li><button type=\"button\" class=\"button button-item\" data-set=\"" + data[i].SGTYPECODE + "\">" + voteUIMap.setSgTypeName(data[i].SGTYPECODE) + "</button></li>");
-                                }
-                            }
-                        } else {
-                            for (var i = 0; i < data.length; i++) {
-                                szHtml.push("<li><button type=\"button\" class=\"button button-item\" data-set=\"" + data[i].SGTYPECODE + "\">" + voteUIMap.setSgTypeName(data[i].SGTYPECODE) + "</button></li>");
-                            }
-                        }
-
-                    }
-
-                    $("#sgList").html('');
-                    $('#sgList').append(szHtml.join(''));
-
-                    $('.dropdown.sg').unbind().removeData();
-                    $('.dropdown.sg').Dropdown();
-                }
-            });
-        },*/
-        /*setSgTypeName: function (code) {
-            var name = "";
-            switch (code) {
-                case 2:
-                    name = "국회의원";
-                    break;
-                case 3:
-                    name = "광역단체장";
-                    break;
-                case 4:
-                    name = "기초단체장";
-                    break;
-                case 5:
-                    name = "광역의원";
-                    break;
-                case 6:
-                    name = "기초의원";
-                    break;
-                case 10:
-                    name = "교육의원";
-                    break;
-                case 11:
-                    name = "교육감";
-                    break;
-            }
-            return name;
-        },*/
-        /*setOpenCleanSelBoxSetting: function (id) {
-            $("#" + id + "List").parent().removeClass('attached');
-            $("#" + id + "List").parent().attr('aria-hidden', 'true');
-            $("#" + id + "List").parent().prevAll('.button-choose').text('선택');
-            $("#" + id + "List").parent().prevAll('.button-choose').attr('data-set', '');
-            $("#" + id + "List").parent().prevAll('.button-choose').removeClass('attached');
-            $("#" + id + "List").parent().prevAll('.button-choose').attr('aria-hidden', 'true');
-            $("#" + id + "List").html('');
-        }*/
+        }
     }
     voteUIMap.init();
 })(jQuery);
