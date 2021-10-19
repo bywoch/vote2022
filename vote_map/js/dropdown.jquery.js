897b3c4cd672904aeb3065f0a9f9b29aa2510227
Bright Chung <bywoch@gmail.com>
2021-10-19 17:00:00 +0900

    vote_map 06

--- vote_map/js/dropdown.jquery.js
+++ vote_map/js/dropdown.jquery.js
@@ -20,66 +20,86 @@
 
         dropdown.init = function () {
             if ($text != '') {
-                //this.setSelectedText($buttonChoose, $text);
+                // 선택된 텍스트가 비어있지 않은 경우 실행
+                var selectChk = false; // 선택 여부를 확인하는 변수
 
-                var selectChk = false;
-
+                // 각 리스트 아이템을 순회하며 선택된 텍스트와 일치하는지 확인
                 $listItems.each(function () {
                     if ($(this).text() == $text) {
-                        selectChk = true;
+                        selectChk = true; // 일치하는 항목이 있을 경우 선택 여부를 true로 설정
 
+                        // 버튼 텍스트와 데이터 속성을 설정하고 다음 단계로 진행
                         $buttonChoose.text($text);
                         $buttonChoose.attr('data-set', $(this).attr('data-set'));
-
                         dropdown.nextStep();
                     }
                 });
 
+                // 선택 여부가 false이고, 특정 조건에 부합하는 경우
                 if (!selectChk && $value[$index] != "none" && ($call == "vote" || $call == "candidate")) {
-                    //dropdown.nextStep();  
+                    // 다음 단계로 이동하는 함수 호출
                     nextStepMove("action", "");
                 }
             }
-            this.bindHandlers();
+            this.bindHandlers(); // 이벤트 핸들러를 등록
         }
+
         dropdown.bindHandlers = function () {
 
-            var self = this;
+            var self = this; // 현재 객체를 저장하기 위한 변수
+
+            // $this 엘리먼트에 클릭 이벤트 핸들러를 등록
             $this.on('click', function (e) {
-                var $target = $(e.target);
+                var $target = $(e.target); // 클릭한 요소를 jQuery 객체로 가져옴
+
+                // 클릭한 요소가 버튼이면서 'button-choose' 클래스를 가지고 있는 경우
                 if ($target && $target.context.nodeName === 'BUTTON' && $target.hasClass('button-choose')) {
-                    self.toggleListExpanded();
-                } else if ($target && $target.context.nodeName === 'BUTTON' && $target.hasClass('button-item')) {
-                    $buttonChoose.text($target.text());
-                    $buttonChoose.attr('data-set', $target.attr('data-set'));
+                    self.toggleListExpanded(); // 리스트 확장/축소를 토글
+                }
+                // 클릭한 요소가 버튼이면서 'button-item' 클래스를 가지고 있는 경우
+                else if ($target && $target.context.nodeName === 'BUTTON' && $target.hasClass('button-item')) {
+                    $buttonChoose.text($target.text()); // 선택한 버튼의 텍스트를 버튼에 설정
+                    $buttonChoose.attr('data-set', $target.attr('data-set')); // 선택한 버튼의 데이터 속성을 설정
 
-                    dropdown.nextStep();
+                    dropdown.nextStep(); // 다음 단계로 진행
 
-                    self.unExpanded();
+                    self.unExpanded(); // 리스트를 축소
                 }
             });
         }
+
+        // 선택된 엘리먼트의 텍스트를 변경하는 함수
         dropdown.setSelectedText = function (element, text) {
             element.text(text);
         }
+
+        // 드롭다운 리스트를 토글하여 확장/축소하는 함수
         dropdown.toggleListExpanded = function () {
-            ($list.hasClass('attached')) ? this.unExpanded(): this.expanded();
+            // 리스트가 이미 확장되어 있다면 축소, 그렇지 않으면 확장
+            ($list.hasClass('attached')) ? this.unExpanded() : this.expanded();
         }
+
+        // 드롭다운 리스트를 확장하는 함수
         dropdown.expanded = function () {
             if (!$list.hasClass('attached')) {
                 $buttonChoose.addClass('expanded');
                 $list.addClass('attached');
-                $list.attr('aria-hidden', 'false');
+                $list.attr('aria-hidden', 'false'); // 스크린 리더 사용자에게 보이도록 함
             }
         }
+
+        // 드롭다운 리스트를 축소하는 함수
         dropdown.unExpanded = function () {
             if ($list.hasClass('attached')) {
                 $buttonChoose.removeClass('expanded');
                 $list.removeClass('attached');
-                $list.attr('aria-hidden', 'true');
+                $list.attr('aria-hidden', 'true'); // 스크린 리더 사용자에게 숨김 처리
             }
         }
+
+        // 다음 단계로 이동하는 함수
         dropdown.nextStep = function () {
+            // 호출 종류에 따라 다음 단계를 수행
             if ($call == "interest") {
                 if ($this.hasClass('interest_sido')) {
                     setInterestGuList($buttonChoose.attr('data-set'));
@@ -87,28 +107,24 @@
                     setInterestDongList($buttonChoose.attr('data-set'));
                 }
             } else if ($call == "candidate") {
-                //if ($this.hasClass('sido')) {
-                //    setGuList($type, $buttonChoose.attr('data-set'));
-                //} else if ($this.hasClass('gu')) {
-                //    setDongList($buttonChoose.attr('data-set'));
-                //}
                 nextStepMove($value[$index], $buttonChoose.attr('data-set'));
             } else if ($call == "vote") {
-                /* if ($this.hasClass('sido')) {
-                     nextStepMove('gu', $buttonChoose.attr('data-set'));
-                 } else if ($this.hasClass('gu')) {
-                     nextStepMove('action', $buttonChoose.attr('data-set'));
-                 }  */
-
                 nextStepMove($value[$index], $buttonChoose.attr('data-set'));
             }
         }
+
+        // 초기화 함수 호출
         dropdown.init();
+
     }
 
+    // jQuery의 확장 메서드를 사용하여 "Dropdown" 플러그인을 정의
     $.fn.Dropdown = function (options) {
+        // 선택한 각 엘리먼트에 대해 동작을 수행
         return this.each(function () {
-            if (undefined == $(this).data('Dropdown')) { //해당 플러그인이 없으면 플러그 인을 달아준다.
+            // 해당 엘리먼트에 "Dropdown" 플러그인 데이터가 없는 경우에만 실행
+            if (undefined == $(this).data('Dropdown')) {
+                // 새로운 "Dropdown" 플러그인 인스턴스를 생성하고 해당 엘리먼트에 연결
                 var plugin = new $.Dropdown(this, options);
                 $(this).data('Dropdown', plugin);
             }
--- vote_map/js/vote.open.shell2.js
+++ vote_map/js/vote.open.shell2.js
@@ -51,7 +51,7 @@ function cleanOpenParam(type) {
 (function ($) {
     var mapPaper = null;
     var initNum = 0;
-    var raphaelDelegate = function () {};
+    var raphaelDelegate = function () { };
     raphaelDelegate.prototype = {
         init: function (e) {
             voteUIMap.init();
@@ -223,7 +223,7 @@ function cleanOpenParam(type) {
                         /*voteUIMap.setSgTypeSelBox();*/ //지방선거 선거구분
                         setNowLocatioInfoAndGePyoRate(paramOpen.sido.name, $(this).attr('data-nav-rate'));
                     });
-                    
+
                 }
             });
         },
@@ -235,21 +235,30 @@ function cleanOpenParam(type) {
                 dataType: 'jsonp',
                 jsonp: 'callback',
                 success: function (data) {
-                    var str = [];
+                    var str = []; // 구 이름과 해당 경로 요소를 저장하는 배열
+
+                    // 메인 HTML 변수 초기화
                     mainHtml = '';
+
+                    // 데이터를 반복하며 구 이름과 해당 경로 요소를 저장
                     for (i = 0; i < data.length; i++) {
                         for (j = 0; j < mapArea.length; j++) {
+                            // 맵 영역 데이터와 현재 데이터의 SIDO가 일치하는 경우
                             if (mapArea[j].seq == 1 && (data[i].SIDO == mapArea[j].seq_code)) {
+                                // 경로 요소의 속성을 설정
                                 attr = {
-                                    fill: data[i].JDColor,
-                                    "stroke": '#d9dbdc', //테두리 컬러
-                                    "stroke-width": 1, //테두리 두께
+                                    fill: data[i].JDColor, // 채우기 색상
+                                    "stroke": '#d9dbdc', // 테두리 색상
+                                    "stroke-width": 1, // 테두리 두께
                                 };
+
+                                // str 배열에 해당 구 이름을 키로 하여 경로 요소를 저장
                                 str[mapArea[j].eng_name] = mapPaper.path(mapArea[j].path).attr(attr);
                             }
                         }
                     }
                 }
+
             });
         },
         setGu: function (url, index) {
@@ -288,18 +297,29 @@ function cleanOpenParam(type) {
                     });
                     /* ~ ↑ 대선추가 ↑ ~ */
 
+                    // 클릭 이벤트 핸들러를 등록
                     $guItems = $('.area-gu-items');
                     $guArea = $guItems.find('.area-gu');
                     $guArea.on('click', function (e) {
-                        e.preventDefault();
+                        e.preventDefault(); // 기본 동작 방지 (링크 클릭 시 페이지 이동 방지)
+
+                        // 현재 클릭한 지역의 인덱스를 가져옴
                         $guIndex = $guArea.index($(this));
+
+                        // 현재 선택된 지역과 같은 지역이면 아무 동작하지 않고 종료
                         if ($guIndex == guCurrent) {
                             return false;
                         }
+
+                        // API URL과 지역명을 가져옴
                         var $apiUrl = $(this).attr('href'),
                             $guText = $(this).text();
+
+                        // "gu" 파라미터를 설정하고 "dong" 파라미터를 초기화
                         setOpenParam("gu", $(this).attr('data-nav'), $(this).attr('data-nav-nm'));
                         cleanOpenParam("dong");
+
+                        // "dong" 리스트를 업데이트하고 선택한 지역을 하이라이트 표시
                         voteUIMap.setDongList($apiUrl);
                         $(this).parent().addClass('area-gu-items-active');
                         $guItems.filter(':eq(' + guCurrent + ')').removeClass('area-gu-items-active');
@@ -307,6 +327,7 @@ function cleanOpenParam(type) {
                         $('.area-dong-body').remove();
 
                         /* ~ ↓ 대선추가 ↓ ~ */
+                        // 선택한 구의 이름을 화면에 표시
                         var selectGu = $(this).text();
                         $('.select-gu .select-top').text(selectGu + ' ▼').append('<span>&#62;</span>');
                         var returnDong = '읍·면·동 ▼'
@@ -314,31 +335,36 @@ function cleanOpenParam(type) {
                         /* ~ ↑ 대선추가 ↑ ~ */
 
                         /*voteUIMap.setSgTypeSelBox();*/ //지방선거 선거구분
+                        // 현재 위치 정보와 게표율을 설정
                         setNowLocatioInfoAndGePyoRate(paramOpen.sido.name + "&nbsp;>&nbsp;" + paramOpen.gu.name, $(this).attr('data-nav-rate'));
                     });
+
+                    // 지도를 그리는 함수를 호출
                     voteUIMap.drawMap(data, index);
+
                 }
             });
         },
         setDongList: function (url) {
             //var current = 0;
+            // AJAX 요청을 통해 지역 정보를 가져옴
             $.ajax({
-                type: 'get',
-                url: url,
-                dataType: 'jsonp',
-                jsonp: 'callback',
-                success: function (data) {
-                    /*mainHtml = '<div class="area-dong-body"><h4><strong class="border">읍·면·동</strong></h4>';*/ //지방선거 지도 하단 읍·면·동 표시 타이틀
+                type: 'get', // GET 요청
+                url: url, // 지정된 URL로 요청을 보냅니다.
+                dataType: 'jsonp', // 데이터 타입을 JSONP로 설정
+                jsonp: 'callback', // JSONP 콜백 파라미터 이름 설정
+                success: function (data) { // 요청이 성공했을 때의 콜백 함수
+                    // 지역 정보를 동적으로 생성하여 HTML에 추가
                     mainHtml = '<div class="area-dong-body">';
                     mainHtml += '<ul id="emdUL">';
                     for (i = 0; i < data.length; i++) {
+                        // 각 지역의 정보를 버튼으로 생성하여 리스트에 추가
                         mainHtml += '<li><button type="button" class="btn-default" data-nav="' + data[i].EMDID + '" data-nav-nm="' + data[i].EMDNMAE + '" data-nav-rate="' + data[i].GEPYO_RATE + '" >' + data[i].EMDNMAE + '</button></li>';
                     }
                     mainHtml += '</ul>';
                     mainHtml += '</div>';
-                    //$('.vote-area-dong').append(mainHtml); //지방선거 지도 하단 읍·면·동 표시
 
-                    /* ~ ↓ 대선추가 ↓ ~ */
+                    // 대선 추가 부분: 선택된 지역 정보를 화면에 표시하고 이벤트 처리를 설정
                     $('.select-dong').append(mainHtml);
                     $('.vote-map .area-gu-items button').contents().unwrap().wrap('<span></span>'); // 지도 클릭 방지 처리
                     $('.select-dong .select-top').on('mouseover focus', function () {
@@ -349,47 +375,50 @@ function cleanOpenParam(type) {
                     $('.select-dong').on('mouseleave', function () {
                         $(this).children('.area-dong-body').hide();
                     });
-                    /* ~ ↑ 대선추가 ↑ ~ */
 
+                    // 각 동을 클릭했을 때의 이벤트 핸들러 설정
                     $dongItems = $('.select-dong .area-dong-body .btn-default')
                     $dongItems.on('click', function () {
-                        /*$index = $dongItems.index($(this));
-                        if ($index == current) {
-                            return false;
-                        }*/
+                        // 선택한 동의 정보를 파라미터로 설정하고 활성화 스타일을 변경
                         setOpenParam("dong", $(this).attr('data-nav'), $(this).attr('data-nav-nm'));
                         $dongItems.removeClass('active');
                         $(this).addClass('active');
-                        $dongItems.filter(':eq(' + current + ')').removeClass('active');
-                        current = $index;
 
-                        /* ~ ↓ 대선추가 ↓ ~ */
+                        // 대선 추가 부분: 선택한 동을 화면에 표시
                         var selectDong = $(this).text();
                         $('.select-dong .select-top').text(selectDong);
-                        /* ~ ↑ 대선추가 ↑ ~ */
 
-                        /*voteUIMap.setSgTypeSelBox();*/ //지방선거 선거구분
+                        // 현재 위치 정보와 게표율을 설정
                         setNowLocatioInfoAndGePyoRate(paramOpen.sido.name + "&nbsp;>&nbsp;" + paramOpen.gu.name + "&nbsp;>&nbsp;" + paramOpen.dong.name, -1);
                     });
                 }
             });
+
         },
+        // drawMap 함수: 지도에 지역 경계를 그리는 함수
         drawMap: function (data, index) {
+            // 이전에 그려진 지도 내용을 모두 지움
             mapPaper.clear();
+
+            // 데이터를 기반으로 지역 경계를 그림
             for (i = 0; i < data.length; i++) {
                 for (j = 0; j < mapArea.length; j++) {
+                    // 지역 정보와 매핑된 경계 정보를 확인하여 그릴 지역을 찾음
                     if (mapArea[j].seq == 2 && (data[i].SIDO == mapArea[j].seq_code) && (mapArea[j].name == data[i].SGNAME)) {
+                        // 경계의 스타일 및 속성을 설정
                         attr = {
-                            fill: data[i].JDColor,
-                            "title": mapArea[j].name,
-                            "stroke": '#d9dbdc', //테두리 컬러
-                            "stroke-width": 1, //테두리 두께
+                            fill: data[i].JDColor, // 채우기 색상
+                            "title": mapArea[j].name, // 툴팁으로 표시될 제목
+                            "stroke": '#d9dbdc', // 테두리 컬러
+                            "stroke-width": 1, // 테두리 두께
                         };
+                        // 경계를 그림
                         mapPaper.path(mapArea[j].path).attr(attr);
                     }
                 }
             }
         }
+
     }
     voteUIMap.init();
 })(jQuery);
