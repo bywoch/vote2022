5a748fe3a2fc8ea2186b308a18c779409cf62343
Bright Chung <bywoch@gmail.com>
2022-02-08 17:00:00 +0900

    graph.js 02

--- m/js/graph.js
+++ m/js/graph.js
@@ -1,21 +1,15 @@
-$(document).ready(function () {
-    //highchart
-    canditimeStampDayMatching();
-    candi2Way1();
-    makeCandiWayChart(candidateSeriesData2Way1);
-});
-
 var candidate_chart;
-var canditimeStampArray = [];
-var candidateSeriesData2Way1 = [];
+var dailyCandi = [];
+
 var candidateColor = {
     심상정: "#f7cc46",
     안철수: "#ce5f2c",
     윤석열: "#d93a35",
     이재명: "#1e4d9b",
-    김동연: "#666",
-    없음: "#aaa"
+    없음: "#aaa",
 };
+var dailyCandiWay = ["이재명", "윤석열", "안철수", "심상정", "없음"];
+
 var candiPlot = [
     {
         value: 1633827599000,
@@ -39,6 +33,7 @@ var candiPlot = [
         }
     },
 ];
+
 var lastCandidateDate = "";
 
 function to_date(date_str) {
@@ -46,25 +41,20 @@ function to_date(date_str) {
     var sYear = yyyyMMdd.substring(0, 4);
     var sMonth = yyyyMMdd.substring(5, 7);
     var sDate = yyyyMMdd.substring(8, 10);
+    //console.log(new Date(Number(sYear), Number(sMonth) - 1, Number(sDate)));
+    //alert("sYear :"+sYear +"   sMonth :"+sMonth + "   sDate :"+sDate);
+
     return Date.UTC(Number(sYear), Number(sMonth) - 1, Number(sDate));
 }
 
-function canditimeStampDayMatching() {
-    $.each(candidateApproveData, function (i, d) {
-        canditimeStampArray[to_date(d.date)] = {
-            year: d.year,
-            month: d.month,
-            week: d.week,
-            week2: d.week2,
-        };
-    });
-}
+$(document).ready(function () {
+    setCandidateLineChart();
+});
 
-function candi2Way1() {
-    var candidateName2Way1 = ["이재명", "윤석열", "안철수", "심상정", "김동연", "없음"];
-    $.each(candidateName2Way1, function (i, cate) {
+function setCandidateLineChart() {
+    $.each(dailyCandiWay, function (i, cate) {
         var legendIsShow = true;
-        candidateSeriesData2Way1.push({
+        dailyCandi.push({
             name: cate,
             data: [],
             visible: legendIsShow,
@@ -77,8 +67,7 @@ function candi2Way1() {
                 lineColor: candidateColor[cate],
             },
         });
-
-        candidateSeriesData2Way1.push({
+        dailyCandi.push({
             name: cate,
             type: "arearange",
             lineWidth: 0,
@@ -92,31 +81,47 @@ function candi2Way1() {
                 enabled: false,
             },
         });
-        //console.log(candidateSeriesData2Way1);
+        dailyCandi.push({
+            name: cate,
+            data: [],
+            type: "scatter",
+            visible: legendIsShow,
+            enableMouseTracking: false,
+            linkedTo: ":previous",
+            opacity: 0.2,
+            color: candidateColor[cate],
+        });
     });
-    $.each(candidateApproveData2Way1, function (j, d) {
-        $.each(candidateName2Way1, function (i, data) {
-            //   console.log(d);
-            if (d[data + "_mean"] != "" && d[data + "_mean"]) {
-                candidateSeriesData2Way1[i * 2].data.push([
-                    to_date(d.date),
-                    d[data + "_mean"],
-                ]);
-                candidateSeriesData2Way1[i * 2 + 1].data.push([
-                    to_date(d.date),
-                    d[data + "_lower"],
-                    d[data + "_upper"],
-                ]);
+
+    // dailyApprove는 아래 링크 로드
+    // http://poll-mbc.co.kr/data/new/dailyApprove.js
+
+    $.each(dailyApprove, function (j, d) {
+        $.each(dailyCandiWay, function (i, data) {
+            if (d["mean-" + data] != "" && d["mean_" + data]) {
+                dailyCandi[i * 3].data.push([to_date(d.date), d["mean_" + data]]);
+                dailyCandi[i * 3 + 1].data.push([
+          to_date(d.date),
+          d["lower_" + data],
+          d["upper_" + data],
+        ]);
             }
         });
-        if (j === -1) {
-            lastCandidateDate = to_date(candidateSeriesData2Way1[j].date);
-        }
     });
-}
 
-function makeCandiWayChart(seriesData) {
-    candidate_chart = Highcharts.chart("candidate-container", {
+    // candidateScatter2Way1Data는 아래 링크 로드
+    // http://poll-mbc.co.kr/data/candidate_scatter_two_1.js
+
+    $.each(candidateScatter2Way1Data, function (i, d) {
+        $.each(dailyCandiWay, function (i, data) {
+            if (d[data] != "") {
+                dailyCandi[i * 3 + 2].data.push([to_date(d.start), d[data]]);
+            }
+        });
+    });
+
+    // new-chart-candidate 이거 바꿔주시면 됩니다.
+    candidate_chart = Highcharts.chart("new-chart-candidate", {
         title: {
             text: "",
         },
@@ -130,16 +135,26 @@ function makeCandiWayChart(seriesData) {
                 formatter: function () {
                     return Highcharts.dateFormat("%y년 %m월 %e일", this.value);
                 },
+                style: {
+                    fontSize: "10px",
+                },
             },
             min: lastCandidateDate - 31536000000,
-            max: lastCandidateDate + 584000000,
+            max: 1647424861000,
+            verticalAlign: 'top',
         },
         chart: {
             spacingLeft: 0,
+            zoomType: "x",
         },
         yAxis: {
             title: {
                 text: null,
+            },
+            labels: {
+                style: {
+                    fontSize: "10px",
+                },
             },
             min: 0,
         },
@@ -154,6 +169,29 @@ function makeCandiWayChart(seriesData) {
                     enabled: false,
                 },
             },
+            scatter: {
+                marker: {
+                    radius: 3,
+                    states: {
+                        hover: {
+                            enabled: true,
+                            lineColor: "rgb(100,100,100)",
+                        },
+                    },
+                },
+                states: {
+                    hover: {
+                        marker: {
+                            enabled: false,
+                        },
+                    },
+                },
+
+                tooltip: {
+                    headerFormat: "<b>{point.y}</b><br>",
+                    pointFormat: "{point.x} cm, {point.y} kg",
+                },
+            },
         },
         tooltip: {
             crosshairs: true,
@@ -165,42 +203,36 @@ function makeCandiWayChart(seriesData) {
             padding: 0,
             borderRadius: 5,
             formatter: function () {
-                // The first returned item is the header, subsequent items are the
-                // points
                 return [
-                    "<div class='hth'><b>" +
-                    canditimeStampArray[this.x].month +
-                    "월 " +
-                    canditimeStampArray[this.x].week +
-                    "주</b></div>",
-                ].concat(
+          "<div class='hth'><b>" + yyyymmdd(this.x) + "</b></div>",
+        ].concat(
                     this.points ?
-                        this.points.map(function (point) {
-                            return (
-                                "<div class='ht candi' style='width:100px; border-color:" +
-                                point.series.color +
-                                "'><span><img src = 'http://poll-mbc.co.kr/img/person/" +
-                                point.series.name +
-                                ".png' class='tooltip-img' alt=''><span>" +
-                                point.series.name +
-                                "</span></span><b style='color:" +
-                                point.series.color +
-                                "'>" +
-                                point.y +
-                                "%</b></div>"
-                            );
-                        }) : []
+                    this.points.map(function (point) {
+                        return (
+                            "<div class='ht candi' style='border-color:" +
+                            point.series.color +
+                            "'><span><img src = 'http://poll-mbc.co.kr/img/person/" +
+                            point.series.name +
+                            ".png' class='tooltip-img'><span>" +
+                            point.series.name +
+                            "</span></span><span style='font-weight:500; color:" +
+                            point.series.color +
+                            "'>" +
+                            point.y +
+                            "%</span></div>"
+                        );
+                    }) : []
                 );
             },
             style: {
                 fontSize: "14px",
             },
-            positioner: function (labelWidth, labelHeight, point) {
+            /*positioner: function (labelWidth, labelHeight, point) {
                 return {
-                    x: point.plotX + 40,
-                    y: point.plotY
-                };
-            },
+                    x: point.plotX + 50,
+                    y: point.plotY + 50,
+                }
+            },*/
         },
         exporting: {
             buttons: {
@@ -209,16 +241,43 @@ function makeCandiWayChart(seriesData) {
                 }
             }
         },
-        series: seriesData,
+        series: dailyCandi,
     });
+
     candidate_chart.xAxis[0].update({
-        min: 1631491200000, //날짜 9월3주 시작 (9월 13일)
-        max: 1647424861000
-    }, // 3월 16일까지
+            min: 1631491200000, //날짜 9월3주 시작 (9월 13일)
+            max: 1647424861000,
+        }, // 3월 16일까지
         true
     );
     //pointer 남기기
     Highcharts.Pointer.prototype.reset = function () {
         return undefined;
     };
+}
+
+function yyyymmdd(timestamp) {
+    var d = new Date(timestamp), // Convert the passed timestamp to milliseconds
+        yyyy = d.getFullYear(),
+        mm = ("0" + (d.getMonth() + 1)).slice(-2), // Months are zero based. Add leading 0.
+        dd = ("0" + d.getDate()).slice(-2), // Add leading 0.
+        hh = d.getHours(),
+        h = hh,
+        min = ("0" + d.getMinutes()).slice(-2), // Add leading 0.
+        ampm = "AM",
+        time;
+
+    if (hh > 12) {
+        h = hh - 12;
+        ampm = "PM";
+    } else if (hh === 12) {
+        h = 12;
+        ampm = "PM";
+    } else if (hh == 0) {
+        h = 12;
+    }
+
+    time = yyyy + "년 " + mm + "월 " + dd + "일";
+
+    return time;
 }
